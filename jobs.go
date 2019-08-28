4afe8f0e6804fb4723c97ee626a88e8e50f11629
dlmc <dongliang.m.chen@gmail.com>
2019-08-28 11:15:31 -0400

    Updating comments

--- jobs.go
+++ jobs.go
@@ -6,7 +6,7 @@ import (
 )
 
 // Parallel executes fnc on each element of the provided slice with go routine
-// max defines the max number of workers to launch, when it's set to 0, the len of the slice is used
+// max defines the max number of workers to launch, when it's set to 0 or less, the len of the slice is used
 func Parallel(slice interface{}, max int, fnc func(i int)) {
 	var w sync.WaitGroup
 	jobs := reflect.ValueOf(slice)
@@ -46,7 +46,7 @@ func Run(funcs ...func()) {
 }
 
 // Do excutes each function in the provided slice concurrently and return when all done
-// max defines the max number of workers to launch, when it's set to 0, the len of the slice is used
+// max defines the max number of workers to launch, when it's set to 0 or less, the len of the slice is used
 func Do(funcs []func(), max int) {
 	var w sync.WaitGroup
 	l := len(funcs)
